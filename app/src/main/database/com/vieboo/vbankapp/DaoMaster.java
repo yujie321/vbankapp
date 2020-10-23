070ffbbd4fa4d5ad864c1ef26b73a780afc4cb70
Administrator <862433824@qq.com>
2020-10-23 16:00:39 +0800

    tij

--- app/src/main/database/com/vieboo/vbankapp/DaoMaster.java
+++ app/src/main/database/com/vieboo/vbankapp/DaoMaster.java
@@ -21,11 +21,13 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
+        PersonImageBeanDao.createTable(db, ifNotExists);
         UserInfoDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
+        PersonImageBeanDao.dropTable(db, ifExists);
         UserInfoDao.dropTable(db, ifExists);
     }
 
@@ -45,6 +47,7 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
+        registerDaoClass(PersonImageBeanDao.class);
         registerDaoClass(UserInfoDao.class);
     }
 
